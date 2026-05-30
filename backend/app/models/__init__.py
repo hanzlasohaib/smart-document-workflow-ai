479bff7edd9b4e93354ea51ecef6ce1636994f76
Hanzla Sohaib <hanzlamaan125@gmail.com>
2026-05-30 16:38:18 +0500

    Fix document workflow tables and notification system

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,5 @@
 from .user import User
 from .document import Document
 from .extracted_field import ExtractedField
+from .automation_log import AutomationLog
+from .notification import Notification
